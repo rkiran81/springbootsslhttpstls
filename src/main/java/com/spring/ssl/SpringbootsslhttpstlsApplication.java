8f195bbd2ec51a84fbd4584fe71b0c575fe7457e
Kiran Kumar Rajanna(UST <U38740@ust.com>
2024-09-07 15:34:46 +0530

    initial commit

---
+++ src/main/java/com/spring/ssl/SpringbootsslhttpstlsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsslhttpstlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsslhttpstlsApplication.class, args);
+	}
+
+}
